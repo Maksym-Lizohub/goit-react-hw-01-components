44cdf9daed94e2f2c794adb7d06676f34f95bb8c
Maksym Lizohub <maksym.lizohub@gmail.com>
2022-07-26 03:14:14 +0300

    update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+import Profile from './Profile/Profile';
+import user from '../data/user.json';
+import { id, label, percentage } from '../data/data.json';
+import Statistics from './Statistics/Statistics';
+
+const { username, tag, location, avatar, stats } = user;
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={username}
+        tag={tag}
+        location={location}
+        avatar={avatar}
+        stats={stats}
+      />
+      <Statistics title={'Upload stats'} stats={(id, label, percentage)} />
     </div>
   );
 };
