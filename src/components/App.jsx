e8b9bed4c711aea2f4845e7afdfaece4411d6a84
Maksym Lizohub <maksym.lizohub@gmail.com>
2022-07-26 04:31:27 +0300

    created friends

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ import Profile from './Profile/Profile';
 import user from '../data/user.json';
 import data from '../data/data.json';
 import Statistics from './Statistics/Statistics';
+import FriendList from './FriendList/FriendList';
+import friends from '../data/friends.json';
 
 const { username, tag, location, avatar, stats } = user;
 
@@ -16,6 +18,7 @@ export const App = () => {
         stats={stats}
       />
       <Statistics title={'upload stats'} stats={data} />
+      <FriendList friends={friends} />;
     </div>
   );
 };
