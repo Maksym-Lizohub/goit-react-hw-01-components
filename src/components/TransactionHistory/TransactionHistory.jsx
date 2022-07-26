cc50ee4ff7404f211693de1b51ba52ea69056514
Maksym Lizohub <maksym.lizohub@gmail.com>
2022-07-26 05:47:34 +0300

    Create TransactionHistory.jsx

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,27 @@
+import TransactionItem from '../TransactionItem/TransactionItem';
+
+const TransactionHistory = ({ items }) => {
+  return (
+    <table className="transaction-history">
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => (
+          <TransactionItem
+            key={id}
+            type={type}
+            amount={amount}
+            currency={currency}
+          />
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default TransactionHistory;
