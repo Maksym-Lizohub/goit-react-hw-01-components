5cfb300a539df709611d9526e9f40e97e8b6a404
Maksym Lizohub <maksym.lizohub@gmail.com>
2022-07-26 04:57:12 +0300

    Update FriendList.jsx

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,3 +1,19 @@
-const FriendList = () => {};
+import FriendListItem from '../FriendListItem/FriendListItem';
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul class="friend-list">
+      {/* <!-- Произвольное кол-во FriendListItem --> */}
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <FriendListItem
+          key={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
+      ))}
+    </ul>
+  );
+};
 
 export default FriendList;
